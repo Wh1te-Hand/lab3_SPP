3fc19530793d96b2f7f1feea559bd8c0c2ce8d70
White Hand <White Hand@DESKTOP-8O2Q7AJ>
2022-12-06 02:55:19 +0300

    Make open file

---
+++ ClassLibrary1/WindowView.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WindowHandler 
+{
+    public class WindowView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void LoadAssembly(object o, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Filter += "Dll files(*.dll)|*.dll";
+            if (fileDialog.ShowDialog() == false)
+                return;
+           // MessageBox.Show(fileDialog.FileName, fileDialog.FileName, MessageBoxButton.OKCancel, MessageBoxImage.Question);
+
+
+        }
+
+    }
+}
--- SPP_lab3/MainWindow.xaml.cs
+++ SPP_lab3/MainWindow.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WindowHandler;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SPP_lab3
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var viewModel = new WindowView();
+            DataContext = viewModel;
+            var commandBinding = new CommandBinding();
+            commandBinding.Command = ApplicationCommands.Open;
+            commandBinding.Executed += viewModel.LoadAssembly;
+            menuItem_Open.CommandBindings.Add(commandBinding);
         }
     }
 }
